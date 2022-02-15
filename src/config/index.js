a2dec9b04736b18a6a12eba8c7039c0509b89502
VikoAltamirano <vikoaltamirano248@gmail.com>
2022-02-15 09:03:33 -0500

    change "=" to ":"

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const config = {
-    mongoUri = process.env.MONGO_URI,
-    port = process.env.PORT,
+    mongoUri: process.env.MONGO_URI,
+    port: process.env.PORT,
 }
 module.exports = config;
